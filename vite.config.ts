abb0e2dca3e102392da57ece014a25aa0c1a3006
Thiha <git@thiha.dev>
2026-05-24 14:03:11 +0700

    fix: build dependency upgrades

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()]
-})
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [react()],
+});
